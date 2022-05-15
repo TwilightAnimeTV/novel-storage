e140c94655c5b30dccdfcd77542b357dcfe12f93
TwilightAnimeTV <95697119+TwilightAnimeTV@users.noreply.github.com>
2022-05-16 05:47:42 +0800

    Add files via upload

--- storage.js
+++ storage.js
@@ -1,6 +1,13 @@
 
   let novel = [
-     { title:"86",sourceLink:"eighty_six_light_novel.html" },
+    {
+          title:"Date This Super Cute Me!",
+         sourceLink:"Date-This-Super-Cute-Me.html"
+    },
+    {
+       title:"86",
+    sourceLink:"eighty_six_light_novel.html"
+     },
     { title:"Classroom Of The Elite",sourceLink:"classroom-of-the-elite.html" }, { title:"The Angel Next Door Spoils Me Rotten",sourceLink:"otonari-no-tenshi-sama-ni-itsu-no-ma-ni-ka-dame-ningen-ni-sareteita-ken-ln.html" },
     {
       title:"Full Clearing Another World under a Goddess with Zero Believers [LN]",sourceLink:"full-clearing-another-world-under-a-goddess-with-zero-believers-light-novel.html"
